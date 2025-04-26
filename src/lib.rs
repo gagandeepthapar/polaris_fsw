fd06be2c1608d4c5a8552ee3d05cb72da367c56e
Gagandeep Thapar <gsthapar@gmail.com>
2025-04-26 13:32:48 -0700

    updating gnc fsw loop with logs, updated inputs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use polaris_log::types::PolarisLogger;
-
 pub mod actuators;
 pub mod control;
 pub mod estimation;
@@ -10,21 +8,27 @@ pub mod sensors;
 use actuators::types::ActuatorBus;
 use control::types::ControlBus;
 use estimation::types::EstimationBus;
-use fsw_types::GNCState;
+use fsw_types::{GNCState, ParamBus};
 use reference::types::ReferenceBus;
 use sensors::types::{RawSensorBus, SensorBus};
 
+use log;
+
+#[derive(Debug)]
 pub struct FlightSoftware {
-    _logger: PolarisLogger,
+    // _logger: Option<PolarisLogger>,
+    _param_bus: ParamBus,
     prev_state: GNCState,
-    curr_state: GNCState,
+    pub curr_state: GNCState,
 }
 
 impl FlightSoftware {
     // Initialize FSW / Consts
-    pub fn initialize(logger: PolarisLogger) -> Self {
+    pub fn initialize(fsw_params: ParamBus) -> Self {
+        log::trace!("Initializing FSW");
         Self {
-            _logger: logger,
+            // _logger: None,
+            _param_bus: fsw_params,
             prev_state: GNCState::default(),
             curr_state: GNCState::default(),
         }
@@ -32,36 +36,50 @@ impl FlightSoftware {
 
     // "GNC Loop" -> outputs Actuator Commands
     pub fn gnc_loop(&mut self, raw_sensor_bus: &mut RawSensorBus) -> ActuatorBus {
+        log::trace!("Running GNC FSW Loop");
+
         // overwrite previous
         std::mem::swap(&mut self.curr_state, &mut self.prev_state);
+        log::trace!("{:?}", raw_sensor_bus);
 
         // read sensors
         self.curr_state.raw_sensor_bus = std::mem::take(raw_sensor_bus);
         self.curr_state.tlm_sensor_bus = SensorBus::process(
+            // Current State
             &self.curr_state.raw_sensor_bus,
+            // Previous State
             &self.prev_state.estimation_bus,
         );
 
         // estimate state
         self.curr_state.estimation_bus = EstimationBus::process(
+            // Current State
             &self.curr_state.tlm_sensor_bus,
+            // Previous State
             &self.prev_state.estimation_bus,
         );
 
         // compute reference
         self.curr_state.reference_bus = ReferenceBus::process(
+            // Current State
             &self.curr_state.estimation_bus,
+            // Previous State
             &self.prev_state.reference_bus,
         );
 
         // compute control error and commands
         self.curr_state.control_bus = ControlBus::process(
+            // Current State
             &self.curr_state.estimation_bus,
             &self.curr_state.reference_bus,
+            // Previous State
         );
 
         // compute actuator commands
-        self.curr_state.actuator_bus = ActuatorBus::process(&self.curr_state.control_bus);
+        self.curr_state.actuator_bus = ActuatorBus::process(
+            // Current State
+            &self.curr_state.control_bus, // Previous State
+        );
 
         self.curr_state.actuator_bus.clone()
     }
